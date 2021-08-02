99338610d6ab81940d4937a44cd4546974d1df65
x612skm <soubhikmitra98@gmail.com>
2021-08-02 16:54:37 +0530

    done with static data

--- insider/src/pages/homepage/Home.js
+++ insider/src/pages/homepage/Home.js
@@ -1,10 +1,18 @@
 import Topbar from "../../components/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
 import "./home.css";
 
 export default function Home () {
     return(
         <>
         <Topbar/>
+        <div className="homeContainer">
+        <Sidebar />
+        <Feed/>
+        <Rightbar/>
+        </div>
         </>
     );
 }
