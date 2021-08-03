8736fa9a5190aef2bc8d35e0c97113fb4f55602a
x612skm <soubhikmitra98@gmail.com>
2021-08-04 00:14:22 +0530

    static site ready

--- insider/src/pages/login/Login.jsx
+++ insider/src/pages/login/Login.jsx
@@ -5,9 +5,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">insider17</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you on insider17.
           </span>
         </div>
         <div className="loginRight">
