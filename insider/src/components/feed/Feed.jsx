5cee91178a4da54eb865fa44fc99e48259fe9726
x612skm <soubhikmitra98@gmail.com>
2021-08-07 21:43:09 +0530

    added the api fetch error in fetch

--- insider/src/components/feed/Feed.jsx
+++ insider/src/components/feed/Feed.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+import axios from "axios";
+
+
+// {import { Posts } from "../../dummyData";}
 
 export default function Feed() {
+  const [posts,setPosts] = useState([])
+  
+  useEffect(() => {
+    const fetchPosts = async() => {
+      const res = await axios.get("posts/timeline/610d95a1c2f0ca583ebc95e9l");
+      console.log(res);
+    };
+    fetchPosts();
+  },[]); 
+
   return (
     <div className="feed">
+ 
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {/* {Posts.map((p) => (
           <Post key={p.id} post={p} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
